16eb3b3f2cb3bd382bffbe1e23618cab12620023
imnotoxy <imnotoxy@users.noreply.github.com>
2017-05-01 09:25:25 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    port: 8080,
-    bots: 0, // Not Implemented
+    port: 3000
     maxConnections: 100,
 	highscoreName: "How to change Highscore Msg",
 	highscoreMsg: "Set this message in the config",
